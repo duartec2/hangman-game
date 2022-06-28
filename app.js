99e86c8ee1ee2f9ed6b6f54e5dc4fbe88590e13c
Ricardo Duarte <duartec2@msn.com>
2022-06-28 12:35:42 -0500

    Clean code

--- app.js
+++ app.js
@@ -1,58 +1,33 @@
 window.app = () => {
-    const options = {
-        lives: 10,
-        tries: 0,
-        started: false,
-        loading: false,
-        word: '',
-        wordDefinition: '',
-        keysPressed: [],
-        alphabet: 'ABCDEFGHIJKLMNÑOPQRSTUVWXYZ',
-        checkWord: ''
-    };
-
-    async function getRandomWord() {
-        try {
-            const response = await fetch('https://palabras-aleatorias-public-api.herokuapp.com/random');
-            if (!response.ok) {
-                console.error(`Something went wrong: ${response.status} - ${response.statusText}`);
-            }
-            return await response.json();
-        } catch (error) {
-            console.log(error);
-            return error;
-        }
-    }
+    const words = [
+        'SERVICIO POSTAL',
+        'EQUIPO DE SONIDO',
+        'CIENCIA FICCIÓN'
+    ];
 
     return {
-        ...options,
+        lives: 5,
+        word: '',
+        keysPressed: [],
+        alphabet: 'ABCDEFGHIJKLMNÑOPQRSTUVWXYZ',
+        checkWord: '',
         start() {
-            //this.reset();
-            this.loading = true;
-            getRandomWord().then(data => {
-                this.word = data.body.Word.toUpperCase();
-                this.checkWord = this.word.replace(/ /g, '');
-                this.wordDefinition = data.body.DefinitionMD;
-                this.loading = false;
-                this.started = true;
-            });
+            this.word = words[Math.floor(Math.random() * words.length)];
+            this.checkWord = this.word.normalize("NFD").replace(/[\u0300-\u036f]/g, "").replace(/ /g, '');
          },
-        reset() {
-           this.options = options; 
-        }, 
-        pressed(letter) {
 
-            this.tries++;
-            this.keysPressed.push(letter);
-            
-            if(this.word.indexOf(letter) === -1) {
+        pressed(char) {
+            this.keysPressed.push(char);
+            this.checkWord = this.checkWord.replace(new RegExp(char, 'gi'), '');
+
+            if(this.word.indexOf(char) === -1) {
                 this.lives--;
-            } else {
-                this.checkWord = this.checkWord.replace(new RegExp(letter, 'gi'), '');
             }
+        },
 
-            console.log(this.checkWord);
-            
+        has(char) {
+            char = char.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+            return this.keysPressed.includes(char);
         },
     }
 };
